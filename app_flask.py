f1b6ad97d2d6586fb0cddae0a6cf92fbbfe6643f
Your Name <you@example.com>
2024-03-01 23:47:18 +0200

    010324

--- app_flask.py
+++ app_flask.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect
-from models import Note, Tag, db_session
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 app.debug = True
